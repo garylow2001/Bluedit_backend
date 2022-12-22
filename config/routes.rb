77ab4897fbbd0e0b2fae2e5106218399d1e3a85d
Gary <garylow2001@gmail.com>
2022-12-22 11:05:56 +0800

    restarted 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'post/index'
-  get 'post/create'
-  get 'post/destroy'
-  get 'post/update'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
