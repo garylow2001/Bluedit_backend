245137b582094eb4b60357d2feec113f9cfb6586
Gary <garylow2001@gmail.com>
2022-12-22 12:02:29 +0800

    added User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  root "home#index"
 end
