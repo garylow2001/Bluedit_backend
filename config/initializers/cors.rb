398142817fd2c336c724fc22dead6a67990380ad
Gary <garylow2001@gmail.com>
2022-12-22 11:03:34 +0800

    Revert "restarted 2"
    This reverts commit 20253a7cad786975c9e40858f8114f6b60761437.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+<<<<<<< HEAD
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
@@ -14,3 +15,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+=======
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "example.com"
+#
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
+>>>>>>> parent of f892bfc (added cors)
