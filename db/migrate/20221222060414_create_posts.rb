70bf72300b208040054e303c40452ab1e73beebe
Gary <garylow2001@gmail.com>
2022-12-28 09:59:35 +0800

    reseted

--- db/migrate/20221222060414_create_posts.rb
+++ db/migrate/20221222060414_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
-      t.string :category
+      t.string :title, null:false
+      t.text :body, null:false
+      t.string :category, null:false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
